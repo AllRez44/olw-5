723385264fd5ba783963358024b4776b931ec5a3
AllRez44 <allanresendegomes@outlook.com>
2024-06-18 10:58:19 -0300

    Update create products migration

--- database/migrations/2024_06_18_135149_create_products_table.php
+++ database/migrations/2024_06_18_135149_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->boolean('is_featured')->default(true);
             $table->timestamps();
         });
     }
