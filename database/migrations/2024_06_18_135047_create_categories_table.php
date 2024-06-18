ebc84eb130033bff6368b21fa3ed432aaca381e0
AllRez44 <allanresendegomes@outlook.com>
2024-06-18 10:56:19 -0300

    Fix create brands & create categories migrations

--- database/migrations/2024_06_18_135047_create_categories_table.php
+++ database/migrations/2024_06_18_135047_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('is_feature')->default('false');
+            $table->boolean('is_feature')->default(false);
             $table->timestamps();
         });
     }
