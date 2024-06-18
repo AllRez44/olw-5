e2df680487783fea5ff1b00d1d13895fcc51cdc3
AllRez44 <allanresendegomes@outlook.com>
2024-06-18 11:02:31 -0300

    Update create products migration

--- database/migrations/2024_06_18_135149_create_products_table.php
+++ database/migrations/2024_06_18_135149_create_products_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('brand_id')->constrained('brands')->nullOnDelete();
+            $table->foreignId('category_id')->constrained('category')->nullOnDelete();
             $table->string('name');
             $table->string('slug');
             $table->text('description');
