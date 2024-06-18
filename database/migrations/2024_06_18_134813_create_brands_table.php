cc0de8c784da18a72490086a71d0199b55e31cea
AllRez44 <allanresendegomes@outlook.com>
2024-06-18 10:52:18 -0300

    Update create brands migration

--- database/migrations/2024_06_18_134813_create_brands_table.php
+++ database/migrations/2024_06_18_134813_create_brands_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('is_featured')->default('false');
             $table->timestamps();
         });
     }
